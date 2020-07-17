a2d3a944e55cbab3388b2a21ff62d01e0b60c8ed
Wouter van den Heuvel <exo_00@hotmail.com>
2020-07-17 14:09:13 +0200

    feat: added bridge

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useMemo } from 'react';
-import content from './content/parseContent';
+import defaultContent from './content/parseContent';
 import Main from './components/Main';
 import IntroModal from './components/introModal/introModal';
 import CompleteModal from './components/completeModal/completeModal';
+import PlayerBridge from './components/playerBridge';
+import { GameData } from './components/playerBridge/GameData';
 import './App.css';
+import { AnyContent } from './common/constants';
 
 function App() {
 
@@ -11,19 +14,25 @@ function App() {
   const [answers, setAnswers] = useState<number[]>([]);
   const [avatar, setAvatar] = useState<string>();
   const [situationIsOpen, setSituationOpen] = useState(false);
+  const [content, setContent] = useState<AnyContent[]>(defaultContent);
 
   const handleRestart = () => {
     setAnswers([]);
   }
 
   const completed = useMemo(() => {
-    return answers.filter(Boolean).length === content.length;
-  }, [answers]);
+    return answers.filter(Boolean).length === content?.length;
+  }, [answers, content]);
 
   const showCompleted = useMemo(() => completed && !situationIsOpen, [completed, situationIsOpen]);
 
+  const handleGameDataReceived = (data: GameData) => {
+    setContent(data.content);
+  }
+
   return (
     <>
+      <PlayerBridge gameDataReceived={handleGameDataReceived}/>
       { intro && (<IntroModal selectedAvatar={avatar} onClose={() => {setIntro(false)}} onChangeAvatar={setAvatar}/>)}
       { !showCompleted && !intro && avatar && <Main content={content} avatar={avatar} answers={answers} setAnswers={setAnswers} setSituationOpen={setSituationOpen}/> }
       { showCompleted && avatar && (<CompleteModal avatar={avatar} restart={handleRestart}/>)}
